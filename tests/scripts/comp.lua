04821d4b945c9fedee486dba495f3cd75b5bcc50
Chris <zornchrisjo@gmail.com>
2023-10-22 18:54:39 +0200

    Update test scripts

--- tests/scripts/comp.lua
+++ tests/scripts/comp.lua
@@ -1,5 +1,6 @@
 local l = "asd"
-print("aa")
+print("aa", "a", "4")
+print(l)
 
 a, b = 8, 5
 
@@ -13,7 +14,24 @@ end
 --repeat
 --until true
 
+ea = { 5, 1, 2, 3 }
 e = { y=5, z=0.345, a="b" }
+
+dummy = 1
+
+ey = e.y
+ey = e["a"]
+ea1 = ea[l]
+
+a = b + c
+a = b + 1
+a = b - c
+a = b / c
+a = b * c
+a = b ^ c
+a = -c
+a = not c
+d = e .. f .. g 
 
 for k, v in e do
 end
@@ -28,7 +46,9 @@ else
     o = 9
 end
 
-function add(x, y)
+function add(x, y, z)
+    local bub = 69
+    local bab = 420
     s = 19
 end
 
@@ -36,10 +56,12 @@ clo = function(u, v)
     w = 12
 end
 
-clo1 = function(u, v) 
+clo1 = function(u) 
     r = 13
 end
 
-clo2 = function(x, a) 
+clo2 = function(x, a, b, c) 
     r = 13
 end
+
+return add(1, 2)
