c344a2b249ccbc2e08eafd0567facd65ea3eb9cf
Chris <zornchrisjo@gmail.com>
2024-01-30 17:46:37 +0100

    Modify call and table

--- tests/scripts/table.lua
+++ tests/scripts/table.lua
@@ -3,7 +3,19 @@ table = NewTable{
 }
 
 index = "x"
-value = table[index]
+index_value = table[index]
 
 list = { 1 , 2 }
 map = { a = 1 , b = 2 }
+
+top = Table {
+    key = 1,
+    value = "",
+
+    nested = Nested {
+        k = "v"
+    },
+
+    fun = "fun"
+
+}
